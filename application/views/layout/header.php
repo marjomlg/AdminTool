c2cc351d01554eac820bab4bb7eb0d009b7d0a61
ChriselynCabs <pasawayc17@gmail.com>
2017-05-10 15:43:48 +0800

    First Push

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -68,6 +68,7 @@
                         </li>
                     <li>
                         <a href="<?php echo base_url('payroll')?>">Payroll</a>
+                        <p>Hello WOrld</p>
                     </li>
                 </ul> 
                 <ul class=" nav navbar-right navbar-nav">

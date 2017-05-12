ce9644c1aabaa7bf0c04fdc70cbc3861d1890d29
marjomlg <marjogorres@gmail.com>
2017-05-12 16:47:35 +0800

    test

--- AdminTool/index.php
+++ AdminTool/index.php
@@ -245,7 +245,7 @@ switch (ENVIRONMENT)
 		if ( ! is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
 		{
 			header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-			echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+			echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct t his: '.SELF;
 			exit(3); // EXIT_CONFIG
 		}
 
